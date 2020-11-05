b227e4d10c2b36d71c5604a501f6cf29c6e360da
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-11-05 11:58:01 -0600

    wip

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -3,10 +3,10 @@ import styled from "styled-components";
 
 export default function Friend({ info, action }) {
   return (
-    <div className="friend">
+    <StyledFriend className="friend">
       {info.name}
       <button onClick={() => action(info.id)}>See details</button>
-    </div>
+    </StyledFriend>
   );
 }
 
