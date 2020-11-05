d91c786bd32537984152f15940eed74f96e32fb0
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-11-05 12:10:57 -0600

    wip

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 export default function Friend({ info, action }) {
   return (
-    <StyledFriend className="friend">
+    <StyledFriend>
       {info.name}
       <button onClick={() => action(info.id)}>See details</button>
     </StyledFriend>
@@ -13,6 +13,9 @@ export default function Friend({ info, action }) {
 const StyledFriend = styled.div`
   /* color: red; */
   font-weight: bold;
+  width: 60%;
+  display: flex;
+  justify-content: space-between;
 
   &:hover {
     color: green;
