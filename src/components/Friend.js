8106c7f54a6f074bd9c20648e3d7b0c62597165a
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-11-05 12:34:04 -0600

    wip

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -11,7 +11,7 @@ export default function Friend({ info, action }) {
 }
 
 const StyledFriend = styled.div`
-  color: ${(pr) => (pr.danger ? pr.theme.primaryColor : pr.theme)};
+  color: ${(pr) => (pr.danger ? pr.theme.primaryColor : pr.themeprimaryColor)};
   font-weight: bold;
   width: 60%;
   display: flex;
